b43966e17955c6451f337bad62ef535ab8bbd114
Nate Tade <ntade@rei.com>
2018-03-07 10:27:28 -0800

    using contrib/rhe/build-netdata-rpm.sh instead of running command directly

--- config/netdata.sh
+++ config/netdata.sh
@@ -3,12 +3,4 @@ cd ~
 rm -rf netdata/
 netdataRelease=${netdataRelease:-master}
 git clone --branch ${netdataRelease} https://github.com/firehol/netdata.git --depth=1
-cd netdata
-autoreconf -ivf
-./configure && make dist
-/bin/cp -f netdata-*.tar.xz netdata.spec ~/rpmbuild/SOURCES/
-#rpmbuild -bb ~/rpmbuild/SOURCES/netdata.spec --without nfacct
-rpmbuild -bb ~/rpmbuild/SOURCES/netdata.spec
-
-# TODO: sign RPM, upload to s3 automagically
-# gpg2 --export-secret-keys --armor your@email.com
+netdata/contrib/rhel/build-netdata-rpm.sh
